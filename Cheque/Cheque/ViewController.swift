1e14887aab88b591afb7d8b83d2358c312736c6f
Kalvin Lam <kalvin.lam5@gmail.com>
2016-02-20 14:11:48 -0800

    Basic layout w/ two views

--- Cheque/Cheque/ViewController.swift
+++ Cheque/Cheque/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var checkButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func checkTask(sender: AnyObject) {
+        
+        checkButton.
+        
+    }
 
 }
 
